55361d733f655c916af0dccb5692a626627a7a23
Hugo Arcos <hugo.arcos@paramq.com>
2025-12-13 00:29:17 -0600

    cambio de api_secret

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
